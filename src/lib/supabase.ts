6a3f8da801edacb159d51b59f8822a45625d9d5e
Bagsoo <jeesoo2928@gmail.com>
2026-02-18 20:41:52 +0900

    환경변수

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://bmsjgqxpqammehuitnsv.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtc2pncXhwcWFtbWVodWl0bnN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1NjkwMjcsImV4cCI6MjA4NjE0NTAyN30.HbWhAir4kWWALGglu_AjSjJwV3bK-LbpmDYPS0PMh4I';
-
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
